e1f4f99fc63dd4f47cbff6f4c669ac75f7c72084
Decomori <tskim275@gmail.com>
2024-10-30 19:51:48 +0900

    🔨 Fix: 특정 캐릭터에서 발생하는 심볼 Rate 관련 오류 수정
    #

--- src/nxapi/type/nxapi-symbol.type.ts
+++ src/nxapi/type/nxapi-symbol.type.ts
@@ -8,9 +8,9 @@ export interface NxapiSymbolInfo {
   symbol_int: string;
   symbol_luk: string;
   symbol_hp: string;
-  symbol_drop_rate: string;
-  symbol_meso_rate: string;
-  symbol_exp_rate: string;
+  symbol_drop_rate?: string;
+  symbol_meso_rate?: string;
+  symbol_exp_rate?: string;
   symbol_growth_count: number;
   symbol_require_growth_count: number;
 }
